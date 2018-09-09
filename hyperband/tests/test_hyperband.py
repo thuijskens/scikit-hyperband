1316dc56c2cf5d8b1333e970c856e6d672b2bbd0
thomashuijskens <thomas.huijskens@quantumblack.com>
2018-09-09 17:48:23 -0300

    Use _run_search function to implement hyperband :rocket:

--- hyperband/tests/test_hyperband.py
+++ hyperband/tests/test_hyperband.py
@@ -13,7 +13,6 @@ def setup():
     param_dist = {'max_depth': [3, None],
                   'max_features': sp_randint(1, 11),
                   'min_samples_split': sp_randint(2, 11),
-                  'min_samples_leaf': sp_randint(1, 11),
                   'bootstrap': [True, False],
                   'criterion': ['gini', 'entropy']}
     
@@ -21,29 +20,6 @@ def setup():
     X, y = digits.data, digits.target
 
     return model, param_dist, X, y, rng
-
-"""
-TODO: This test fails due to the random state not being properly fixed
-
-def test_hyperband():
-    model, param_dist, X, y, rng = setup()
-    search = HyperbandSearchCV(model, param_dist, random_state=rng)
-    search.fit(X, y)
-
-    # results = pd.DataFrame(search.cv_results_)
-    expected_params = {
-        'bootstrap': False,
-        'criterion': 'entropy',
-        'max_depth': None,
-        'max_features': 7,
-        'min_samples_leaf': 2,
-        'min_samples_split': 2,
-        'n_estimators': 81
-    }
-
-    # assert(results.shape[0] == 186) TODO: sort out what the expected n_i and r_i values are
-    assert(search.best_params_ == expected_params)
-"""
 
 
 def test_multimetric_hyperband():
@@ -65,7 +41,7 @@ def test_multimetric_hyperband():
 
     assert('mean_test_roc_auc' in search.cv_results_.keys())
     assert('mean_test_accuracy' in search.cv_results_.keys())
-    # assert(search.best_params_ == results[results.rank_test_roc_auc == 1].params.values)
+    assert (len(search.cv_results_['hyperband_bracket']) == 187)
 
 
 def test_min_resource_param():
